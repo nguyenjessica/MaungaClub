32a6795d7d58d9a85f9994710cbdaf09e0b9e67f
nguyenjessica <133342505+nguyenjessica@users.noreply.github.com>
2023-08-27 19:33:32 +1200

    Add files via upload

--- Maunga Club/script.js
+++ Maunga Club/script.js
@@ -4,3 +4,28 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky, window.scrollY > 60")
 
 })
+
+document.querySelector('.image-input').addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      document.execCommand('insertHTML', false, '<br><br>');
+    }
+  });
+
+  const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+  };
+
+  ScrollReveal().reveal(".chalet__card", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  
+  ScrollReveal().reveal(".chalet__card__content", {
+    ...scrollRevealOption,
+    interval: 500,
+    delay: 200,
+  });
+  
